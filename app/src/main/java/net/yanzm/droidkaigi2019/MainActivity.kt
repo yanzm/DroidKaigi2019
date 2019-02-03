16bb84c68b3d6c04a2784d1a6a3778e914e5a45a
Yuki Anzai <anzai.y.aa@gmail.com>
2019-02-03 12:10:05 +0900

    initial

---
+++ app/src/main/java/net/yanzm/droidkaigi2019/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.yanzm.droidkaigi2019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
